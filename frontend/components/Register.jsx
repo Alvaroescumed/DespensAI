97e860963f3a7a345e63f1283768e826c4cd7533
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-08-15 13:22:52 +0200

    update register form e implementacion de seleccion de imagen

--- frontend/components/Register.jsx
+++ frontend/components/Register.jsx
@@ -1,13 +1,56 @@
 import { useNavigation } from '@react-navigation/native'
-import { Text, View, Button } from "react-native"
+import { useState } from 'react'
+import { Text, View, Button, TextInput } from "react-native"
+import * as ImagePicker from 'expo-image-picker'
 
 export default function Register (){
 
+    const [userData, setUserData ] = useState({
+        fname : "",
+        lname : "",
+        email: "",
+        username: "",
+        birthDay: "",
+        password: "",
+        rpassword: "",
+        bio: "",
+        pfp: null,
+        location: ""
+    })
+
     const navigation = useNavigation()
+
+    async function pickImage(){
+        let res = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+
+        if(!res.canceled){
+            setUserData( prevState => ({
+                ...prevState,
+                pfp: res.assets[0].uri
+            }))
+        }
+    }
+
+    function handleOnChange(e){
+        const {name , value} = e.target
+
+        setUserData({
+            ...userData,
+            [name]: value,
+        })
+    }
 
     return(
         <View>
             <Text>Completa el registro</Text>
+            <TextInput
+                value={userData.fname}
+                onChange={() => handleOnChange()}/>
         </View> 
     )
 }
