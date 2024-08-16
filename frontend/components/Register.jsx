64dc0c1b9c15bfe8bdaa7def67f69a4c967e57ce
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-08-16 10:50:00 +0200

    🔧update - formulario de registro e implementacion date picker

--- frontend/components/Register.jsx
+++ frontend/components/Register.jsx
@@ -1,16 +1,18 @@
 import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
 import { Text, View, Button, TextInput } from "react-native"
+import DateTimePicker from '@react-native-community/datetimepicker'
 import * as ImagePicker from 'expo-image-picker'
 
 export default function Register (){
 
+    const [showDatePicker, setShowDatePicker] = useState(false)
     const [userData, setUserData ] = useState({
         fname : "",
         lname : "",
         email: "",
         username: "",
-        birthDay: "",
+        birthDay: new Date(), 
         password: "",
         rpassword: "",
         bio: "",
@@ -36,9 +38,8 @@ export default function Register (){
         }
     }
 
-    function handleOnChange(e){
-        const {name , value} = e.target
 
+    function handleOnChange(name, value){
         setUserData({
             ...userData,
             [name]: value,
@@ -50,7 +51,34 @@ export default function Register (){
             <Text>Completa el registro</Text>
             <TextInput
                 value={userData.fname}
-                onChange={() => handleOnChange()}/>
+                placeholder='Nombre'
+                onChange={(value) => handleOnChange('fname', value)}/>
+            <TextInput
+                value={userData.lname}
+                placeholder='Apellidos'
+                onChange={(value) => handleOnChange('lname', value)}/>
+            <TextInput
+                value={userData.username}
+                placeholder='Username'
+                onChange={(value) => handleOnChange('username', value)}/>
+            <TextInput
+                value={userData.email}
+                placeholder='email'
+                onChange={(value) => handleOnChange('email', value)}/>
+            <Button title='Fecha de Nacimiento' onPress={() => setShowDatePicker(true)} />
+                {showDatePicker && (
+                    <DateTimePicker
+                        value={userData.birthDay}
+                        mode='date'
+                        display='default'
+                        onChange={(e, selectedDate) =>{
+                            setShowDatePicker(false)
+                            handleOnChange('birthDay', selectedDate)
+                        }}
+                    />
+                )}
+            < Button title='Selecciona tu imagen de perfil' onPress={pickImage} />
+
         </View> 
     )
 }
