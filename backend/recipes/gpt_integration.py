f4359bc244a0f3c803eb7876242b6dc4c751341f
Alvaro Escudero <135117878+Alvaroescumed@users.noreply.github.com>
2024-08-17 18:07:45 +0200

    🔧 update - IA fix 2.0

--- backend/recipes/gpt_integration.py
+++ backend/recipes/gpt_integration.py
@@ -1,5 +1,4 @@
-
-import requests
+from huggingface_hub import InferenceClient
 import os
 from dotenv import load_dotenv
 
@@ -8,33 +7,46 @@ load_dotenv()
 api_key = os.getenv('AI_KEY')
 
 def generate_recipe(ingredients: str, preferences: str) -> str:
-    prompt = (
-        f"Crea una receta de cocina usando los siguientes ingredientes: {ingredients}, incluyendo los pasos y el tiempo de preparacion en la respuesta. Ademas ten en cuenta que la receta cumpla {preferences} "
+    # prompt = (
+    #     f"Crea una receta de cocina usando los siguientes ingredientes: {ingredients}, incluyendo los pasos y el tiempo de preparacion en la respuesta. Ademas ten en cuenta que la receta cumpla {preferences} "
+    # )
+
+    # # API endpoint del modelo que vamos a usar de Hugging Face
+    # api_url = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1/v1/chat/completions"
+
+    # headers = {
+    #     "Authorization": f"Bearer {api_key}"
+    # }
+
+    # payload = {
+    #     "inputs": prompt,
+    #     "parameters": {
+    #         "max_tokens": 500,
+    #         "num_return_sequences": 1,
+    #         "temperature": 0.7 ,
+    #         "model" : 'mistralai/Mixtral-8x7B-Instruct-v0.1'
+    #     }
+    # }
+
+    # response = requests.post(api_url, headers=headers, json=payload)
+
+    client = InferenceClient(
+        "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        token= api_key,
     )
+    #  
+    for message in client.chat_completion(
+        messages=[{"role": "user", "content": "Crea una receta de cocina usando los siguientes ingredientes: pollo, cebolla, ajo, limón, arroz, incluyendo los pasos y el tiempo de preparacion en la respuesta. Ademas ten en cuenta que la receta cumpla baja en sodio, sin gluten  y sin lactosa"}],
+        max_tokens=1000,
+        stream=True,
+    ):
+        print(message.choices[0].delta.content, end="")
 
-    # API endpoint del modelo que vamos a usar de Hugging Face
-    api_url = "https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407"
-
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-
-    payload = {
-        "inputs": prompt,
-        "parameters": {
-            "max_length": 100000,
-            "num_return_sequences": 1,
-            "temperature": 0.7 
-        }
-    }
-
-    response = requests.post(api_url, headers=headers, json=payload)
-
-    if response.status_code == 200:
-        result = response.json()
-        return result[0]['generated_text']
-    else:
-        return f"Error: {response.status_code}, {response.text}"
+    # if response.status_code == 200:
+    #     result = response.json()
+    #     return result
+    # else:
+    #     return f"Error: {response.status_code}, {response.text}"
 
 
 if __name__ == "__main__":
